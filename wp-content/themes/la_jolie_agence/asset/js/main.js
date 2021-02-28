81ce48ac32a627e2a1aca502d4266b2a13e77dc5
QVan27 <qvannarath@gmail.com>
2021-03-01 00:50:14 +0100

    modif js commentaire

--- wp-content/themes/la_jolie_agence/asset/js/main.js
+++ wp-content/themes/la_jolie_agence/asset/js/main.js
@@ -1,31 +1,26 @@
 $(window).load(function () {
-  //   $(".page-flexslider").flexslider({
-  //     animation: "slide",
-  //     directionNav: false,
-  //   });
-
+  // Parallax
   $(".parallax-window").parallax({
     imageSrc: "wp-content/themes/la_jolie_agence/asset/img/fleur.jpg",
   });
 
-//   var fullscreen = function () {
-//     var fheight = $(window).height();
-//     $(".fullscreen").css("height", fheight);
-//   };
+  //   var fullscreen = function () {
+  //     var fheight = $(window).height();
+  //     $(".fullscreen").css("height", fheight);
+  //   };
 
-//   //Execute on load
-//   fullscreen();
+  //   //Execute on load
+  //   fullscreen();
 
-//   //Execute on window resize
-//   $(window).resize(function () {
-//     fullscreen();
-//   });
-
- 
-    $('.flexslider').flexslider({
-        animation: "slide",
-        directionNav: false,
-        controlNav: false,
-    });
-
+  //   //Execute on window resize
+  //   $(window).resize(function () {
+  //     fullscreen();
+  //   });
+  
+//   Flexslider
+  $(".flexslider").flexslider({
+    animation: "slide",
+    directionNav: false,
+    controlNav: false,
+  });
 });
