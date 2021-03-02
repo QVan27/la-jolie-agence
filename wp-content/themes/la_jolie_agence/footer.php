81fad2bca84bbce965ccb43c64570963c32f65e6
Oliebat <bationo.c@gmail.com>
2021-03-02 21:32:04 +0100

    footer-final

--- wp-content/themes/la_jolie_agence/footer.php
+++ wp-content/themes/la_jolie_agence/footer.php
@@ -14,7 +14,42 @@
 <!-- footer -->
 <div class="push"></div>
 </div><!-- #wrap -->
-<footer class="footer"></footer>
+<footer class="footer">
+    <div class="wrapper">
+        <div class="minibox">
+            <h3 class="title-footer">La Jolie Agence</h3>
+            <hr>
+            <p class="text-footer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ex error atque sit alias distinctio laboriosam? Reprehenderit, sit et quisquam odio facere unde harum, esse quos ipsa cupiditate aliquid aut?</p>
+        </div>
+
+        <div class="vertical-footer"></div>
+
+        <div class="minibox">
+            <h3 class="title-footer">Contactez-nous</h3>
+            <hr>
+            <div class="box-call">
+                <i class="fas fa-phone-square-alt"></i><a class="text-footer" href="tel:+33711223344">O711223344</a>
+                <br>
+                <br>
+                <i class="fas fa-envelope"></i><a class="text-footer">LaJolieAgence@gmail.com</a>
+            </div>
+
+        </div>
+
+        <div class="vertical-footer"></div>
+
+        <div class="minibox">
+            <h3 class="title-footer">Suivez-nous</h3>
+            <hr>
+            <a href=""><i class="fab fa-facebook-square fa-2x"></i></a>
+            <a href=""></i><i class="fab fa-instagram fa-2x"></i></a>
+            <a href=""><i class="fab fa-youtube fa-2x"></i></a>
+
+
+        </div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
